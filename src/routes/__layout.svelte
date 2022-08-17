38536408a53a8495bfb1d37f8cbe16e5d3ba21d9
Trevor Vick <cam3ra22@gmail.com>
2022-08-16 22:01:24 -0700

    added Week Record component and added me page icon on bottom navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,16 +8,15 @@
 <div class="btm-nav">
 	<button>
 		<svg
+			width="24px"
+			height="24px"
+			viewBox="0 0 200 200"
+			data-name="Layer 1"
+			id="Layer_1"
 			xmlns="http://www.w3.org/2000/svg"
-			class="h-5 w-5"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+			fill="currentColor"
+			><title /><path
+				d="M169,170.71l-2-4a73.78,73.78,0,0,0-31-32,66.14,66.14,0,0,0,29-54.5,65,65,0,1,0-101,54,73.78,73.78,0,0,0-31,32l-2,4a10.06,10.06,0,0,0,18,9l2-4a55,55,0,0,1,98,0l2,4a10,10,0,0,0,13.5,4.5c5-2,7-8,4.5-13ZM55,80.21a45,45,0,1,1,45,45A44.81,44.81,0,0,1,55,80.21Z"
 			/></svg
 		>
 	</button>
