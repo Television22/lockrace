0be8b86aabafe0b68a0b4f6080fb4d491ffcb449
Tim Vick <t.vick@yahoo.com>
2022-08-21 18:52:44 -0700

    fix: pull out bottom nav to component

---
+++ src/components/BottomNav.svelte
@@ -0,0 +1,54 @@
+<div class="btm-nav">
+	<button class="active">
+		<svg
+			width="24px"
+			height="24px"
+			viewBox="0 0 200 200"
+			data-name="Layer 1"
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="currentColor"
+			><title /><path
+				d="M169,170.71l-2-4a73.78,73.78,0,0,0-31-32,66.14,66.14,0,0,0,29-54.5,65,65,0,1,0-101,54,73.78,73.78,0,0,0-31,32l-2,4a10.06,10.06,0,0,0,18,9l2-4a55,55,0,0,1,98,0l2,4a10,10,0,0,0,13.5,4.5c5-2,7-8,4.5-13ZM55,80.21a45,45,0,1,1,45,45A44.81,44.81,0,0,1,55,80.21Z"
+			/></svg
+		>
+		<span class="btm-nav-label">Me</span>
+	</button>
+	<button>
+		<svg
+			width="24px"
+			height="24px"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="feather feather-plus-circle"
+			><circle cx="12" cy="12" r="10" /><line x1="12" y1="8" x2="12" y2="16" /><line
+				x1="8"
+				y1="12"
+				x2="16"
+				y2="12"
+			/></svg
+		>
+		<span class="btm-nav-label">Pick</span>
+	</button>
+	<button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+			/></svg
+		>
+		<span class="btm-nav-label">Leaderboard</span>
+	</button>
+</div>
