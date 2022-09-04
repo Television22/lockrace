54a8061f4b692855edb6f891853d57bd698910a2
Trevor Vick <cam3ra22@gmail.com>
2022-09-04 15:34:46 -0700

    added cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,18 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface PrivateEnv {}
-	// interface PublicEnv {}
-	// interface Session {}
-	// interface Stuff {}
+  // interface Locals {}
+  interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace;
+    };
+    context: {
+      waitUntil(promise: Promise<any>): void;
+    };
+    caches: CacheStorage & { default: Cache }
+  }
+  // interface PrivateEnv {}
+  // interface PublicEnv {}
+  // interface Session {}
+  // interface Stuff {}
 }
